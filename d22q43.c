86319d6fd02ed2f4a621369d118eed62b37a504d
kanganaa28 <Kangana.33330@stu.upes.ac.in>
2026-09-19 23:41:07 +0530

    Add Strong Number checker in d22q43.c
    Implement a program to check if a number is a Strong Number.

---
+++ d22q43.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,temp,digit,i,fact,sum=0;
+
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    temp=n;
+
+    while(n>0)
+    {
+        digit=n%10;
+        fact=1;
+
+        for(i=1;i<=digit;i++)
+            fact=fact*i;
+
+        sum=sum+fact;
+        n=n/10;
+    }
+
+    if(sum==temp)
+        printf("Strong Number");
+    else
+        printf("Not Strong Number");
+
+    return 0;
+}
