c7b75053e198ea2721fccb0ce67f515519e7019a
kanganaa28 <Kangana.33330@stu.upes.ac.in>
2026-09-19 23:17:52 +0530

    Add palindrome checking program in d16q32.c

---
+++ d16q32.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,temp,rev=0,digit;
+
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    temp=n;
+
+    while(n>0)
+    {
+        digit=n%10;
+        rev=rev*10+digit;
+        n=n/10;
+    }
+
+    if(temp==rev)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
