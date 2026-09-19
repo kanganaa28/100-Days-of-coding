8561e7b3b76f3fd51ebcb470e5b7b7d160ca7b5d
kanganaa28 <Kangana.33330@stu.upes.ac.in>
2026-09-19 23:06:03 +0530

    Add quadratic equation solver in d9q17.c

---
+++ d9q17.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a,b,c,d,r1,r2;
+
+    printf("Enter a,b,c: ");
+    scanf("%f%f%f",&a,&b,&c);
+
+    d=b*b-4*a*c;
+
+    if(d>0)
+    {
+        r1=(-b+sqrt(d))/(2*a);
+        r2=(-b-sqrt(d))/(2*a);
+        printf("Two real roots: %.2f %.2f",r1,r2);
+    }
+    else if(d==0)
+    {
+        r1=-b/(2*a);
+        printf("Equal roots: %.2f",r1);
+    }
+    else
+        printf("No real roots");
+
+    return 0;
+}
