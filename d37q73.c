1de979f2b4f745d5f3ca9d4709050293ad696cdd
kanganaa28 <Kangana.33330@stu.upes.ac.in>
2026-09-20 16:50:29 +0530

    Update matrix sum calculation to use row sums
    Replaced single sum variable with an array to store sums of each row.

--- d37q73.c
+++ d37q73.c
@@ -2,7 +2,8 @@
 
 int main()
 {
-    int a[10][10],r,c,i,j,sum=0;
+    int a[10][10],sum[10];
+    int r,c,i,j;
 
     printf("Enter rows and columns: ");
     scanf("%d%d",&r,&c);
@@ -11,14 +12,19 @@ int main()
 
     for(i=0;i<r;i++)
     {
+        sum[i]=0;
+
         for(j=0;j<c;j++)
         {
             scanf("%d",&a[i][j]);
-            sum=sum+a[i][j];
+            sum[i]=sum[i]+a[i][j];
         }
     }
 
-    printf("Sum=%d",sum);
+    printf("Row sums:\n");
+
+    for(i=0;i<r;i++)
+        printf("%d ",sum[i]);
 
     return 0;
 }
