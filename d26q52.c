3ff8f5149088584ab9609f7f9a3467ca1a3cfddb
kanganaa28 <Kangana.33330@stu.upes.ac.in>
2026-09-20 16:26:51 +0530

    Add a simple C program to print asterisks

---
+++ d26q52.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    printf("*\n\n");
+
+    for(i=1;i<=5;i++)
+        printf("*\n");
+
+    printf("\n");
+
+    for(i=1;i<=5;i++)
+        printf("*\n");
+
+    printf("\n*");
+
+    return 0;
+}
